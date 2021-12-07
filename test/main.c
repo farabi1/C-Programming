bd001580fd18d5651c85a185a3270a305951d428
farabi1 <rashidfarabi@gmail.com>
2021-12-07 15:59:12 +0600

    Test Commit

--- test/main.c
+++ test/main.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+# include <stdio.h>
+void dbl ( int * ) ;
+void tple ( int * ) ;
+void qdpl ( int * ) ;
+int main( )
 {
-    printf("C Program expert\n");
-    return 0;
+    int  num = 2, i ;
+    void ( *p[ ] )( int * ) = { dbl, tple, qdpl } ;
+    for ( i = 0 ; i < 3 ; i++ )
+    {
+        p[ i ]( &num ) ;
+        printf ( "%d\n", num ) ;
+    }
+    return 0 ;
+}
+
+void dbl ( int *n )
+{
+    *n = *n * *n ;
+}
+
+void tple ( int *n )
+{
+    *n = *n * *n * *n ;
+}
+
+void qdpl ( int *n )
+{
+    *n = *n * *n * *n * *n ;
 }
